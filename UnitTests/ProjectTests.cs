6c32f42594269e286fb37313b49784a1609d0798
beck410 <beck410@hotmail.com>
2015-02-05 15:18:56 -0600

    Set up unit tests for classes

---
+++ UnitTests/ProjectTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projects;
+
+namespace UnitTest {
+  [TestClass]
+  public class ProjectTests {
+    [TestMethod]
+    public void TestProjectConstructor() {
+      Project project = new Project("simpleShapes");
+      Assert.AreEqual("simpleShapes",project.Name);
+    }
+    
+    [TestMethod]
+    [ExpectedException (typeof(ArgumentException))]
+    public void TestProjectConstructorWithSpaces() {
+      Project project = new Project("simple shapes");
+    }
+  }
+}
+
