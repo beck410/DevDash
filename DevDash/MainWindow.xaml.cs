dba42320bef352d47abe8293f8e4139d443fef2f
beck410 <beck410@hotmail.com>
2015-02-05 17:05:07 -0600

    Added UI Testing

--- DevDash/MainWindow.xaml.cs
+++ DevDash/MainWindow.xaml.cs
@@ -20,23 +20,29 @@ namespace DevDash {
   public partial class MainWindow : Window {
     public MainWindow() {
       InitializeComponent();
+      Check_Current_Project_List_State(true);
+      Check_Past_Project_List_State(true);
+    }
+
+    public void Current_Projects_Button_Click(object sender, RoutedEventArgs e) {
 
     }
 
-    private void Current_Projects_Button_Click(object sender, RoutedEventArgs e) {
+    public void Past_Projects_Button_Click(object sender, RoutedEventArgs e) {
 
     }
 
-    private void Past_Projects_Button_Click(object sender, RoutedEventArgs e) {
-
+    public void Main_New_Project_Button_Click(object sender, RoutedEventArgs e) {
+      // TODO: open new window
+      Main_New_Project_Button.IsEnabled = false;
     }
 
-    private void Main_New_Project_Button_Click(object sender, RoutedEventArgs e) {
-
+    public void Check_Current_Project_List_State(bool projectsExist) {
+      Current_Projects_Button.IsEnabled = projectsExist;
     }
 
-    private void check_Current_Project_List_State() {
-
+    public void Check_Past_Project_List_State(bool projectsExist) {
+      Past_Projects_Button.IsEnabled = projectsExist;
     }
   }
 }
