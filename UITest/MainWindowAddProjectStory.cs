7a86df0f87bf7ad18abb2d0fec6b434495056869
beck410 <beck410@hotmail.com>
2015-02-05 15:18:01 -0600

    initial state UITest setup and passing

--- UITest/MainWindowAddProjectStory.cs
+++ UITest/MainWindowAddProjectStory.cs
@@ -1,42 +1,48 @@
-﻿using System;
+﻿﻿using System;
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestStack.White;
 using TestStack.White.Factory;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.WindowItems;
-using DevDash;
+using TestStack.BDDfy;
 
-namespace InitialState {
+namespace UITest {
   [TestClass]
-  public class InitialState {
+  public class MainWindowAddProjectStory {
 
-    private static TestContext test_context;
     private static Window window;
     private static Application application;
-    private static Button Current_Projects;
-    private static Button Past_Projects;
-    private static Button New_Project;
-
-
+    private static Button New_Project_Button;
 
     [ClassInitialize]
-    public static void Setup(TestContext _context) {
-      test_context = _context;
+    public static void Setup(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext _context){
       var applicationDir = _context.DeploymentDirectory;
       var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\DevDash\\bin\\Debug\\DevDash");
       application = Application.Launch(applicationPath);
       window = application.GetWindow("MainWindow", InitializeOption.NoCache);
-      Current_Projects = window.Get<Button>("Current_Projects_Button");
-      Past_Projects = window.Get<Button>("Past_Projects_Button");
-      New_Project = window.Get<Button>("Main_New_Project_Button");
+      New_Project_Button = window.Get<Button>("Main_New_Project_Button");
     }
 
+    void WhenTheNewProjectButtonIsClicked() {
+      New_Project_Button.Click();
+    }
+
+    void GivenTheMainWindowIsOpen() {
+      Assert.IsTrue(window.IsFocussed);
+    }
+
+    void ThenTheNewProjectWindowShouldOpen() {
+      //New Window
+    }
+
+    void AndThenNewProjectcreateButtonIsDisabled() {
+      Assert.IsFalse(New_Project_Button.Enabled);
+    } 
+
     [TestMethod]
-    public void TestZeroState() {
-      Assert.IsTrue(Current_Projects.Enabled);
-      Assert.IsTrue(Past_Projects.Enabled);
-      Assert.IsTrue(New_Project.Enabled);
+    public void MainPageAddNewProjectButtonTest(){
+      this.BDDfy();
     }
 
     [ClassCleanup]
