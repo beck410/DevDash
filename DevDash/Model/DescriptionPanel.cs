6a1fa19cb3d99d81329c0ff16a15f414e411379e
beck410 <beck410@hotmail.com>
2015-02-12 13:37:10 -0600

    added description class properties

---
+++ DevDash/Model/DescriptionPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Model {
+  class DescriptionPanel {
+
+    public int DescriptionId { get; set; }
+    public int ProjectId { get; set; }
+    public string Description { get; set; }
+
+
+    public DescriptionPanel(string description, int projectId) {
+      this.ProjectId = projectId;
+      this.Description = description;
+    }
+  }
+}
