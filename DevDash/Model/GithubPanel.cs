14bf246f2e4fed2a723553d3d0a14389562d912e
beck410 <beck410@hotmail.com>
2015-02-12 13:40:37 -0600

    added githubpanel class and properties

---
+++ DevDash/Model/GithubPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Model {
+  public class GithubPanel {
+     
+    public int GithubId { get; set; }
+    public string link { get; set; }
+    public int ProjectId { get; set; }
+
+    public GithubPanel(string link, int projectId) {
+      this.link = link;
+      this.ProjectId = projectId;
+    }
+  }
+}
