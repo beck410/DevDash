f3169eec5fadf44ad5b7bb63418a7375d4a4e39e
beck410 <beck410@hotmail.com>
2015-02-12 13:41:07 -0600

    added notes class and properties

---
+++ DevDash/Model/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Model {
+  class Note {
+
+    public int NoteId { get; set; }
+    public int NotesPanelId { get; set; }
+    public string NoteDetails { get; set; }
+
+    public Note(int notePanelId, string noteDetails) {
+      this.NotesPanelId = notePanelId;
+      this.NoteDetails = noteDetails;
+    }
+  }
+}
