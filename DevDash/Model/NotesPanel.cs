79e2e88428b5489cf6b482b25ebd36648b1750da
beck410 <beck410@hotmail.com>
2015-02-12 13:41:31 -0600

    added notespanel class and properties

---
+++ DevDash/Model/NotesPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Model {
+  class NotesPanel {
+
+    public int NotesPanelId { get; set; }
+    public int ProjectId { get; set; }
+
+    public NotesPanel(int projectId) {
+      this.ProjectId = projectId;
+    }
+
+  }
+}
