2997183709b506356f66961532dc42fbf6859002
beck410 <beck410@hotmail.com>
2015-02-12 18:17:07 -0600

    added DbContext as reference and created context class to hook up to database

---
+++ DevDash/ProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using DevDash.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash {
+  class ProjectContext : DbContext {
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<NotesPanel> NotesPanels { get; set; }
+    public DbSet<Note> Notes { get; set; }
+  }
+}
