fe4340bf0a8c1fe44c011d722b2578c16543a65d
beck410 <beck410@hotmail.com>
2015-02-12 18:12:05 -0600

    restructured tests to separate unit tests and user tests

--- TestDevDash/Properties/AssemblyInfo.cs
+++ TestDevDash/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyTitle("TestDevDash")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyProduct("TestDevDash")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1cc85d7-7604-4e31-84c7-69581ddc25e6")]
+[assembly: Guid("337ac27d-84f2-48e5-82aa-fefea6c0892f")]
 
 // Version information for an assembly consists of the following four values:
 //
