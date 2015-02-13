0d765a4c9ba39757eb7bb9c721d1c97f1ec00944
beck410 <beck410@hotmail.com>
2015-02-13 16:08:21 -0600

    set up notes panel repo and test structure

---
+++ DevDash/Repositories/NotesPanelRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevDash.Model;
+using System.Data.Entity;
+
+namespace DevDash.Repositories {
+  public class NotesPanelRepository : INotesPaneRepository {
+
+    private ProjectContext _dbContext;
+    public NotesPanelRepository() {
+      _dbContext = new ProjectContext();
+      _dbContext.Projects.Load();
+    }
+
+    public NotesPanel GetPanelDescription(int project_id) {
+      throw new NotImplementedException();
+    }
+
+    public NotesPanel GetById(int id) {
+      throw new NotImplementedException();
+    }
+
+    public void Add(NotesPanel panel) {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(int id) {
+      throw new NotImplementedException();
+    }
+
+    public void Clear() {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ TestDevDash/RepoTests/NotesPanelRepoTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevDash.Repositories;
+using DevDash.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+
+
+namespace TestDevDash.RepoTests {
+  [TestClass]
+  public class NotesPanelRepoTest {
+    private static NotesPanelRepository repo;
+    
+    [ClassInitialize]
+    public static void SetUp(TestContext _context){
+      repo = new NotesPanelRepository();
+      repo.Clear();
+    }
+
+    [ClassCleanup]
+    public static void CleanUp() {
+      repo.Clear();
+    }
+
+    [TestCleanup]
+    public void ClearDB() {
+      repo.Clear();
+    }
+
+    [TestMethod]
+    public void TestMethod1() {
+    }
+  }
+}
