03a841ec72a08592b3bf6392ee31aa6044ca31a6
beck410 <beck410@hotmail.com>
2015-02-13 15:56:01 -0600

    set up description panel repo and test structure

---
+++ DevDash/Repositories/DescriptionPanelRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Repositories {
+  public class DescriptionPanelRepository : IDescriptionPanelRepository{
+
+
+    public Model.DescriptionPanel GetPanelDescription(int project_id) {
+      throw new NotImplementedException();
+    }
+
+    public Model.DescriptionPanel GetById(int id) {
+      throw new NotImplementedException();
+    }
+
+    public void Add(Model.DescriptionPanel description) {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(int id) {
+      throw new NotImplementedException();
+    }
+
+    public void Clear() {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ DevDash/Repositories/IDescriptionPanelRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevDash.Model;
+
+namespace DevDash.Repositories {
+  interface IDescriptionPanelRepository {
+
+    DescriptionPanel GetPanelDescription(int project_id);
+    DescriptionPanel GetById(int id);
+    void Add(DescriptionPanel description);
+    void Delete(int id);
+    void Clear();
+  }
+}
