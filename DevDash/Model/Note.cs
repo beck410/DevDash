af7d6bfe28085f866f6498e635e9d0df6994951d
beck410 <beck410@hotmail.com>
2015-02-12 21:19:33 -0600

    added notes constructor with passing tests

--- DevDash/Model/Note.cs
+++ DevDash/Model/Note.cs
@@ -7,15 +7,23 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace DevDash.Model {
-  class Note {
+  public class Note {
 
     public int NoteId { get; set; }
     public int NotesPanelId { get; set; }
     public string NoteDetails { get; set; }
 
-    public Note(int notePanelId, string noteDetails) {
+    public Note(string noteDetails, int notePanelId) {
+
+      _isEmpty(noteDetails);
+
       this.NotesPanelId = notePanelId;
       this.NoteDetails = noteDetails;
+    }
+
+    private void _isEmpty(string note) {
+    if (string.IsNullOrWhiteSpace(note))
+      throw new ArgumentException("no description provided"); 
     }
   }
 }
---
+++ TestDevDash/UnitTests/TestNote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevDash.Model;
+
+namespace TestDevDash.UnitTests {
+  [TestClass]
+  public class TestNote {
+    [TestMethod]
+    public void TestNoteConstructorWithValidParams() {
+      Note note = new Note("this is a new note about the project", 1);
+      Assert.AreEqual("this is a new note about the project", note.NoteDetails);
+      Assert.AreEqual(1, note.NotesPanelId);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TestNoteConstructorWithEmptyNoteDetail() { 
+      Note note = new Note("", 1);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TestNoteConstructorWithWhiteSpaceForNoteDetail() {
+      Note note = new Note("        ", 1);
+    }
+  }
+}
