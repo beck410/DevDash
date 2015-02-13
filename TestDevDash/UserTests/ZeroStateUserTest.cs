2e99b1c70c94998bb59e4b543bcff2917d263105
beck410 <beck410@hotmail.com>
2015-02-13 12:34:08 -0600

    created test structure for zero state tests

---
+++ TestDevDash/UserTests/ZeroStateUserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White;
+using System.IO;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.Finders;
+
+namespace TestDevDash.UserTests {
+  [TestClass]
+  public class ZeroStateUserTest {
+    
+    [TestMethod]
+    public void TestZeroStateWithNoProjectsInDB() {
+      
+    }
+    
+    [TestMethod]
+    public void TestZeroStateWithOnlyCurrentProjectsInDB() {
+    }
+
+    [TestMethod]
+    public void TestZeroStateWithOnlyPastProjectsInDB() {
+    }
+
+    [TestMethod]
+    public void TestZeroStateWithCurrentAndPastProjectsInDB() {
+    }
+  }
+}
