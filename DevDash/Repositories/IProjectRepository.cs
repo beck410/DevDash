1143c02b75ade8e68262561f124bd04bae73b456
beck410 <beck410@hotmail.com>
2015-02-12 18:16:22 -0600

    added project repo interface and class with initial methods

---
+++ DevDash/Repositories/IProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDash.Repositories {
+  public interface IProjectRepository {
+
+    int GetCount();
+    void Add(Project P);
+    void Delete(Project P);
+    void Clear();
+    Project GetById(int id);
+  }
+}
---
+++ DevDash/Repositories/ProjectsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevDash.Model;
+using System.Data.Entity;
+
+namespace DevDash.Repositories {
+  class ProjectsRepository : IProjectRepository{
+
+    public ProjectsRepository(){
+      _dbContext = new ProjectContext();
+      _dbContext.Projects.Load();
+    }
+
+    //set up for db local
+    private ProjectContext _dbContext;
+
+
+
+    //Project DB Methods
+    public int GetCount(){
+      throw new NotImplementedException();
+    }
+
+    public void Add(Project P) {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(Project P) {
+      throw new NotImplementedException();
+    }
+
+    public void Clear() {
+      throw new NotImplementedException();
+    }
+
+    public Project GetById(int id) {
+      throw new NotImplementedException();
+    }
+  }
+}
