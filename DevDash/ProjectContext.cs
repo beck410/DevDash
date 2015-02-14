2ac499e180ce7539b9b09c4b5775748c025f197d
beck410 <beck410@hotmail.com>
2015-02-13 18:22:24 -0600

    ProjectContext now public class

--- DevDash/ProjectContext.cs
+++ DevDash/ProjectContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DevDash {
-  class ProjectContext : DbContext {
+  public class ProjectContext : DbContext {
     public DbSet<Project> Projects { get; set; }
     public DbSet<NotesPanel> NotesPanels { get; set; }
     public DbSet<Note> Notes { get; set; }
