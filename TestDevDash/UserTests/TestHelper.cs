3df81b99b87d13962c7546786236343cc8559df8
beck410 <beck410@hotmail.com>
2015-02-13 18:21:45 -0600

    added setup and teardown methods for tests

---
+++ TestDevDash/UserTests/TestHelper.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevDash.Model;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White;
+using System.IO;
+using System.Reflection;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems;
+using DevDash.Repositories;
+using DevDash;
+
+namespace TestDevDash.UserTests {
+  class TestHelper {
+
+    private static TestContext test_context;
+    protected static Window window;
+    private static Application application;
+    private static ProjectsRepository repo = new ProjectsRepository();
+    private static ProjectContext context;
+    private static String applicationPath;
+
+    public static void SetUpClass(TestContext _context) {
+      var applicationDir = _context.DeploymentDirectory;
+      applicationPath = Path.Combine(applicationDir, "..\\..\\..\\TestWaitForIt\\bin\\Debug\\DevDash");
+    }
+
+    public static void TestPrep() {
+      application = Application.Launch(applicationPath);
+      window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+      context = repo.Context(); 
+    }
+
+    public static void CleanThisUp() {
+      window.Close();
+      application.Close();
+      repo.Clear();
+    }
+  }
+}
