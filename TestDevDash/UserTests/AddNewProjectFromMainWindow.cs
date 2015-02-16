553ae13e1530633d5c080e43d92c917cc3f2116d
beck410 <beck410@hotmail.com>
2015-02-16 15:01:16 -0600

    set up tests for adding projects from main window

---
+++ TestDevDash/UserTests/AddNewProjectFromMainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDevDash.UserTests {
+  [TestClass]
+  public class AddNewProjectFromMainWindow : TestHelper {
+
+    [ClassInitialize]
+    public static void Setup(TestContext _context) {
+      TestHelper.SetUpClass(_context);
+    }
+
+    [TestInitialize]
+    public void SetUpTests() {
+      TestHelper.TestPrep();
+    }
+
+    [TestCleanup]
+    public void CleanUp() {
+      TestHelper.CleanThisUp();
+    }
+
+    [TestMethod]
+    public void AddFirstProjectToDB() {
+      throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void AddProjectToDBWithExistingProjects() { 
+      throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void AddProjectWithValidData() { 
+      throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void AddProjectWithNoName() { 
+      throw new NotImplementedException();
+    }
+
+    [TestMethod]
+    public void AddProjectWithInvalidArguments() { 
+      throw new NotImplementedException();
+    }
+  }
+}
