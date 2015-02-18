c66b7ab9bde90fdc52820c5d72839d252d097054
beck410 <beck410@hotmail.com>
2015-02-17 21:14:42 -0600

    set up delete projects tests

---
+++ TestDevDash/UserTests/DeleteProjects.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDevDash.UserTests {
+  [TestClass]
+  public class DeleteProjects : TestHelper {
+    
+    [ClassInitialize]
+    public static void Setup(TestContext _context) {
+      TestHelper.SetUpClass(_context);
+    }
+
+    [TestInitialize]
+    public void SetUpTests() {
+      TestHelper.TestPrep();
+    }
+
+    [TestCleanup]
+    public void CleanUp() {
+      TestHelper.CleanThisUp();
+    }
+
+    [TestMethod]
+    public void DeletePastProject() {
+      GivenThereAreXProjects("past");
+      WhenIClick("Past_Projects_Button");
+      ThenIAmOnCurrentProjectsList();
+      AndIShouldSeeXNumberOfProjectsInXListBox(3,"Past_Projects_Listbox","past");
+      WhenIClickXItemButton(0,"Past_Projects_Listbox","Delete_Past_Project_Button"); 
+      ThenIShouldSeeXNumberOfProjectsInXListBox(2,"Past_Projects_Listbox","past");
+      AndDBShouldHaveXProjects(2);
+    }
+
+    [TestMethod]
+    public void DeleteCurrentProject() {
+      GivenThereAreXProjects("current");
+      WhenIClick("Current_Projects_Button");
+      ThenIAmOnCurrentProjectsList();
+      AndIShouldSeeXNumberOfProjectsInXListBox(3,"Current_Projects_Listbox","current");
+      WhenIClickXItemButton(0,"Current_Projects_Listbox","Delete_Current_Project_Button"); 
+      ThenIShouldSeeXNumberOfProjectsInXListBox(2,"Current_Projects_Listbox","current");
+      AndDBShouldHaveXProjects(2);
+
+    }
+  }
+}
