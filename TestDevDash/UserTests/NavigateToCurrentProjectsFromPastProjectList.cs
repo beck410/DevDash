8302db4edbc160a6d8a6c2ae80c849331c01a02c
beck410 <beck410@hotmail.com>
2015-02-17 18:15:40 -0600

    change test to reflect xaml

--- TestDevDash/UserTests/NavigateToCurrentProjectsFromPastProjectList.cs
+++ TestDevDash/UserTests/NavigateToCurrentProjectsFromPastProjectList.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestDevDash.UserTests {
   [TestClass]
-  public class BackToMainMenuFromPastProjectList : TestHelper {
+  public class NavigateToCurrentProjectsFromPastProjectList : TestHelper {
 
    [ClassInitialize]
     public static void Setup(TestContext _context) {
@@ -20,23 +20,27 @@ namespace TestDevDash.UserTests {
       TestHelper.CleanThisUp();
     }
     [TestMethod]
-    public void BackToMenuFromCurrentProjectWhenThereAreProjects() {
-      GivenThereAreXProjects("past");
-      AndIAmOnCurrentProjectsList();
-      WhenIClick("Past_Projects_Back_Button");
+    public void CurrentProjectsListFromPastProjectWhenThereAreCurrentProjects() {
+      GivenThereAreXProjects("past");      
+      WhenIClick("Current_Projects_Button");
+      ThenIAmOnCurrentProjectsList();
+      WhenIClick("");
       ThenIShouldNotSee("Past_Projects_Listbox");
       AndIShouldSee("Current_Projects_Button");
       AndIShouldSee("Past_Projects_Button");
+      AndIShouldSeeXNumberOfProjectsInXListBox(3,"Current_Projects_Listbox","current");
     }
 
     [TestMethod]
-    public void BackToMenuFromCurrentProjectsWhenNoProjects() {
+    public void CurrentProjectsFromCurrentProjectsWhenNoProjects() {
       GivenThereAreNoXProjects("past");
-      AndIAmOnCurrentProjectsList();
+      WhenIClick("Current_Projects_Button");
+      ThenIAmOnCurrentProjectsList();
       WhenIClick("Past_Projects_Back_Button");
       ThenIShouldNotSee("Past_Projects_Listbox");
       AndIShouldSee("Current_Projects_Button");
       AndIShouldSee("Past_Projects_Button");
-    }
+      AndIShouldSeeXNumberOfProjectsInXListBox(0,"Current_Projects_Listbox","current");
+    } 
   }
 }
---
+++ TestDevDash/UserTests/NavigateToPastProjectsFromCurrentProjectsList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDevDash.UserTests {
+  [TestClass]
+  public class NavigateToPastProjectsFromCurrentProjectsList : TestHelper {
+
+    [ClassInitialize]
+    public static void Setup(TestContext _context) {
+      TestHelper.SetUpClass(_context);
+    }
+
+    [TestInitialize]
+    public void SetUpTests() {
+      TestHelper.TestPrep();
+    }
+
+    [TestCleanup]
+    public void CleanUp() {
+      TestHelper.CleanThisUp();
+    }
+    [TestMethod]
+    public void PastProjectsListFromCurrentProjectsWhenThereAreCurrentProjects() {
+      GivenThereAreXProjects("past");
+      AndIAmOnCurrentProjectsList();
+      WhenIClick("View_Past_Projects_Button");
+      ThenIShouldNotSee("Current_Projects_Listbox");
+      AndIShouldSee("Delete_Past_Projects_Button");
+      AndIShouldSee("View_Current_Projects_Button");
+      AndIShouldSeeXNumberOfProjectsInXListBox(3,"Past_Projects_Listbox","past");
+    }
+
+    [TestMethod]
+    public void PastProjectsListFromCurrentProjectsWhenNoProjects() {
+      GivenThereAreNoXProjects("past");
+      AndIAmOnCurrentProjectsList();
+      WhenIClick("Current_Projects_Back_Button");
+      ThenIShouldNotSee("Current_Projects_Listbox");
+      AndIShouldSee("Current_Projects_Button");
+      AndIShouldSee("Past_Projects_Button");
+      AndIShouldSeeXNumberOfProjectsInXListBox(0,"Past_Projects_Listbox","past");
+    }
+  }
+}
