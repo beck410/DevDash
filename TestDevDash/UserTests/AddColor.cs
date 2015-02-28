7c5dad18b05660ce839bab7b024cba18d74f7429
beck410 <beck410@hotmail.com>
2015-02-28 17:20:24 -0600

    user test for adding color

---
+++ TestDevDash/UserTests/AddColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDevDash.UserTests{
+  [TestClass]
+  public class AddColor : TestHelper {
+
+   [ClassInitialize]
+    public static void Setup(TestContext _context) {
+      TestHelper.SetUpClass(_context);
+    }
+
+    [TestInitialize]
+    public void SetUpTests() {
+      TestHelper.TestPrep();
+    }
+
+    [TestCleanup]
+    public void CleanUp() {
+      TestHelper.CleanThisUp();
+    }
+
+    [TestMethod]
+    public void AddNewColorToProjectWhenNoColorsExist() {
+       GivenThereAreXProjects("current");
+      WhenIClick("Current_Projects_Button");
+      ThenIAmOnCurrentProjectsList();
+      AndIShouldSeeXNumberOfProjectsInXListBox(3,"Current_Projects_Listbox","current");
+      WhenISelect(0,"Current_Projects_Listbox");
+      AndIClick("View_Current_Project_Button");
+      AndTextBlockShouldBe("Single_Project_Name","angular_project");
+      WhenIClick("Add_Color_Button");
+      ThenIShouldSeeInModal("AddColorModal","Modal_New_Color_Button");
+      AndIFillColorNameInModal();
+      AndIFillHexCodeInModal();
+      AndIClickInModal("AddColorModal","Modal_New_Color_Button");
+      ThenIShouldSee("Colors_Listbox");
+      AndIShouldSeeXNumberOfItemsInColorListBox();
+    } 
+  }
+}
